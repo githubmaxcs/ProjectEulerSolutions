af528eefdb14dc8efe6d4c9f96e03699a886fed2
githubmaxcs <111184693+githubmaxcs@users.noreply.github.com>
2025-04-02 13:15:41 -0500

    Update P1.ps1

--- P1.ps1
+++ P1.ps1
@@ -1,4 +1,24 @@
 function P1{
+    <#
+    .SYNOPSIS
+    P1 will return the sum of the multiples of two numbers less than a certain limit.
+
+    .DESCRIPTION
+    Given two positive integers in $Numbers, P1 will sum the multiples of the two numbers less than the $Limit, where $Limit is a positive integer. If $Limit is less than each individual number in $Numbers, P1 will return 0.
+
+    .PARAMETER $Numbers
+    An array of two numbers.
+
+    .PARAMETER $Limit
+    The upper bound (not inclusive) for summing multiples of numbers in $Numbers.
+
+    .EXAMPLE
+    P1 -Numbers 3,5 -Limit 1000
+    $Limit has a default value of 1000.
+
+    .OUTPUTS
+    P1 will return an Int32.
+    #>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true)]
@@ -11,8 +31,7 @@ function P1{
     $Sum = 0
     $i = 1
     while ($i -lt $Limit) {
-        if ($i % $Numbers[0] -eq 0) {$Sum += $i}
-        elseif ($i % $Numbers[1] -eq 0) {$sum += $i}
+        if (($i % $Numbers[0] -eq 0) -or ($i % $Numbers[1] -eq 0)) {$Sum += $i}
         $i ++
     }
     return $Sum
